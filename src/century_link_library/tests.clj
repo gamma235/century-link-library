2d489ee0f96179c2737eb8a00548c4866a9fb325
Jesse <jesseluisdiaz@gmail.com>
2016-05-17 18:46:53 -0500

    refactored unit tests to contain all test data within each case

--- src/century_link_library/tests.clj
+++ src/century_link_library/tests.clj
@@ -2,36 +2,40 @@
   (:require [century-link-library.core :as core])
   (:use clojure.test))
 
-;; stub data:
-;;___________
-
-;;should total 7200
-(def sample-graph {:manager {:manager {:manager [:developer {:manager [:developer :developer :qa :qa]} :developer :qa :qa]}}})
-
-;; should total 11,100
-(def sample-graph-long [{:manager {:manager {:manager [:developer :developer :qa :qa]}}},
-                        {:manager {:manager [:qa {:manager [:developer :developer :qa :qa]} :qa :qa]}},
-                        {:manager [:developer :qa]}])
-
-;; begin tests
-;;____________
-
+;; Unit Tests
 (deftest single-manager-total
-  (is (= 7200 (core/total-for-manager sample-graph))))
+  (let [sample-graph {:manager
+                      {:manager
+                       {:manager
+                        [:developer {:manager [:developer :developer :qa :qa]} :developer :qa :qa]}}}]
+    (is (= 7200 (core/total-for-manager sample-graph)))))
 
 (deftest single-not-multiple-manager-total
-  (is (try (core/total-for-manager sample-graph-long)
-        (catch AssertionError e true))))
+  (let [sample-graph-long [{:manager {:manager {:manager [:developer :developer :qa :qa]}}},
+                           {:manager {:manager [:qa {:manager [:developer :developer :qa :qa]} :qa :qa]}},
+                           {:manager [:developer :qa]}]]
+    (is (try (core/total-for-manager sample-graph-long)
+          (catch AssertionError e true)))))
 
 (deftest multiple-manager-totals
-  (is (= '(3900 5400 1800) (core/totals-for-managers sample-graph-long))))
+  (let [sample-graph-long [{:manager {:manager {:manager [:developer :developer :qa :qa]}}},
+                           {:manager {:manager [:qa {:manager [:developer :developer :qa :qa]} :qa :qa]}},
+                           {:manager [:developer :qa]}]]
+    (is (= '(3900 5400 1800) (core/totals-for-managers sample-graph-long)))))
 
 (deftest multiple-not-single-manager-total
-  (is (try (core/totals-for-managers sample-graph)
-        (catch AssertionError e true))))
+  (let [sample-graph {:manager
+                      {:manager
+                       {:manager
+                        [:developer {:manager [:developer :developer :qa :qa]} :developer :qa :qa]}}}]
+    (is (try (core/totals-for-managers sample-graph)
+          (catch AssertionError e true)))))
 
 (deftest multiple-managers-sum
-  (is (= 11100 (core/sum-total-for-managers sample-graph-long))))
+  (let [sample-graph-long [{:manager {:manager {:manager [:developer :developer :qa :qa]}}},
+                           {:manager {:manager [:qa {:manager [:developer :developer :qa :qa]} :qa :qa]}},
+                           {:manager [:developer :qa]}]]
+    (is (= 11100 (core/sum-total-for-managers sample-graph-long)))))
 
 ;; uncomment below expr and and evaluate to run tests
 ;;(run-tests)
